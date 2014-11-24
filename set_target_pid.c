c23fe6a4fd585de459cbd1ea9d73b5715a89f73d
user <user@localhost.localdomain>
2014-11-24 01:59:05 -0500

    Fixed for militant OSes and cleaned logic.

--- set_target_pid.c
+++ set_target_pid.c
@@ -18,13 +18,24 @@
  *
  **********************************************************************************************************************/
 
+#include <errno.h>
 #include <error.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+
+// Some versions of Linux seem to have a "max fork()s for a process".
+// I can't find exactly where this is set, so for now, here is a 
+// TUNABLE constant that can be used to control how often the parent dies.
+// Fork() then killing parent is very processer intensive. Fork() then
+// killing child is not. So, kill as many children as TUNABLE allows,
+// then kill the parent to reset the OS max fork() per proc count.
+#define TUNEABLE 1000
 
 
 int main(int argc, char **argv){
@@ -32,6 +43,12 @@ int main(int argc, char **argv){
 	char *short_name;
 	pid_t target, current, last;
 	int flag;
+	int count;
+	
+	char pid_max_buffer[64];
+	int pid_max_fd;
+	pid_t pid_max;
+	
 
 
 	if((short_name = strrchr(argv[0], '/')) == NULL){
@@ -47,34 +64,65 @@ int main(int argc, char **argv){
 		exit(-1);
 	}
 
+	memset(pid_max_buffer, 0, 64);
+	if((pid_max_fd = open("/proc/sys/kernel/pid_max", O_RDONLY)) == -1){
+		error(-1, errno, "open(\"/proc/sys/kernel/pid_max\")");
+	}
+
+	if(read(pid_max_fd, pid_max_buffer, sizeof(pid_max_buffer)) == -1){
+		error(-1, errno, "read(%d, %p, %d)", pid_max_fd, pid_max_buffer, (int) sizeof(pid_max_buffer));
+	}
+	close(pid_max_fd);
+
+	pid_max = (pid_t) strtol(pid_max_buffer, NULL, 10);
 
 	target = (pid_t) strtol(argv[1], NULL, 10);
 
+	if(target > pid_max){
+		error(-1, 0, "error: target is greater than pid_max! Quitting.");
+	}
 
 	flag = 0;
 	last = 0;
 	current = getpid();
-	while((current < target) || (!flag &&(target < current))){
+	count = 0;
 
-		// Catch the case where the target specified is bigger than /proc/sys/kernel/pid_max .
-		if(flag && (current < target) && (last < target)){
-			fprintf(stderr, "%s error:\tTarget unreachable. Quitting.\n", short_name);
-			exit(-1);
-		}
+	if(current < target){
+		flag = 1;
+	}
+
+	while((flag && (current < target)) || (!flag && (target < current))){
 
 		last = current;
-		if(!(current = fork())){
-			exit(-1);
+		if(count == TUNEABLE){
+
+			if((current = fork()) == -1){
+				exit(-1);
+			}else if(current){
+				exit(0);
+			}
+
+			count = 0;
+			current = getpid();
+
+		}else{
+
+			if(!(current = fork())){
+				exit(-1);
+			}
+
 		}
 
 		if(current < last){
 			if(current == -1){
-				fprintf(stderr, "fork error.\n");
+				printf("error: last pid: %d\n", last);
 				exit(-1);
 			}
 
 			flag = 1;
 		}
+
+		count++;
 	}
 
 	return(0);
